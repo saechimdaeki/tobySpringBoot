0c052d185fa754cd19b9053377cae0be8c775810
Junseong kim <anima94@kakao.com>
2023-01-20 14:43:07 +0900

    서블릿 기술로 프론트 컨트롤러로 전환 및 Hello 컨트롤러 매핑과 바인딩

--- src/main/kotlin/me/saechimdaeki/tobyspringboot/HelloBootApplication.kt
+++ src/main/kotlin/me/saechimdaeki/tobyspringboot/HelloBootApplication.kt
@@ -3,6 +3,7 @@ package me.saechimdaeki.tobyspringboot
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory
 import org.springframework.boot.web.servlet.ServletContextInitializer
 import org.springframework.http.HttpHeaders
+import org.springframework.http.HttpMethod
 import org.springframework.http.HttpStatus
 import org.springframework.http.MediaType
 import javax.servlet.http.HttpServlet
@@ -18,16 +19,31 @@ fun main(args: Array<String>) {
     TomcatServletWebServerFactory().apply {
         val webServer = this.getWebServer(ServletContextInitializer { servletContext ->
 
+            // 이건 스프링에서 빈으로 등록안한다는 가정하에 이렇게 하는 것
+            val helloController = HelloController()
             // 서블릿 등록
-            servletContext.addServlet("hello", object : HttpServlet() {
+            servletContext.addServlet("frontcontroller", object : HttpServlet() {
                 override fun service(req: HttpServletRequest, resp: HttpServletResponse) {
-                    val name = req.getParameter("name")?: "SAE_CHIM_DAEKI"
+                    // 인증, 보안, 다국어, 공통 기능
+                    when {
+                        req.requestURI == "/hello" && req.method == HttpMethod.GET.name -> {
+                            val name = req.getParameter("name") ?: "SAE_CHIM_DAEKI"
+                            val returnValue = helloController.hello(name)
 
-                    resp.status = HttpStatus.OK.value()
-                    resp.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_PLAIN_VALUE)
-                    resp.writer.println("Hello $name")
+                            resp.status = HttpStatus.OK.value()
+                            resp.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_PLAIN_VALUE)
+                            resp.writer.println(returnValue)
+                        }
+                        req.requestURI == "/user" -> {
+                            //
+                        }
+                        else -> {
+                            resp.status = HttpStatus.NOT_FOUND.value()
+                        }
+                    }
                 }
-            }).addMapping("/hello") //hello로 들어오는 요청은 이 서블릿이 처리함.
+            }).addMapping("/*") // 프론트 컨트롤러로 전환
+//                .addMapping("/hello") //hello로 들어오는 요청은 이 서블릿이 처리함.
         })
 
         webServer.start() // 톰캣이 실행됨
